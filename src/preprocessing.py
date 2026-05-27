dc4aeafaf754148e8dcca4972f63207e9f2401f8
Kamsy Umeaku <kamsyumeaku@gmail.com>
2026-05-27 13:49:40 -0400

    implement image preprocessing transforms

---
+++ src/preprocessing.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader
+
+DATA_DIR = Path(__file__).parent.parent / "data" / "raw" / "the_wildfire_dataset_2n_version"
+# transform the data to tensors for train and test 
+train_transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(10),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
+
+test_transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+])
+
+# gets the image from the data folder 
+train_dataset = datasets.ImageFolder(DATA_DIR / "train", transform=train_transform)
+test_dataset = datasets.ImageFolder(DATA_DIR / "test", transform=test_transform)
+
+# Loads the dataset
+train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=64, shuffle=False)
