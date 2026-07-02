e05c776912f9eb9dea959a9fef74feae79d2c77a
Kamsy Umeaku <kamsyumeaku@gmail.com>
2026-07-02 16:01:57 -0400

    chore: adding logging to the binary dataset

--- data/wildfire_dataset.py
+++ data/wildfire_dataset.py
@@ -1,13 +1,16 @@
 import kagglehub
 from pathlib import Path
 import shutil
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 path = kagglehub.dataset_download("elmadafri/the-wildfire-dataset")
-print("Path to dataset files:", path)
+logging.info("Path to dataset files: %s", path)
 # create the path
 destination = Path(__file__).parent / "raw"
 destination.mkdir(parents=True, exist_ok=True)
 # move path to the raw folder
 for item in Path(path).iterdir():
     shutil.move(str(item), destination)
-print("Dataset moved to data/raw/")
+logging.info("Dataset moved to data/raw/")
