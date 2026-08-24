af9594a402cb2eb3ac46bd13fb67d5ba926721b8
Kamsy Umeaku <kamsyumeaku@gmail.com>
2026-08-24 18:15:41 -0400

    feat: added the person/animal detection algorithm to the drone dectection algorithm

--- src/live_drone.py
+++ src/live_drone.py
@@ -1,41 +1,44 @@
 import cv2
 import logging
-from inference import predict_frame, load_model, DEFAULT_MODEL_PATH
+from src.inference import predict_frame, load_model, DEFAULT_MODEL_PATH
+from src.detection.person_animal_detect import detect_person_animal
+from pathlib import Path
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
+VIDEO_PATH = Path(__file__).resolve().parent / "drone.mp4"
+camera_index = str(VIDEO_PATH)# Change this index based on your drone camera feed
+camera = cv2.VideoCapture(camera_index) 
 
-def drone_camera_loop(camera_index=0):
-    camera = cv2.VideoCapture(camera_index)  # Placeholder for drone camera feed
+if not camera.isOpened():
+    logging.error("Drone camera can't be opened")
+    exit()  # Use exit() instead of return since this is not inside a function
 
-    if not camera.isOpened():
-        logging.error("Drone camera can't be opened")
-        return
+logging.info("Drone camera loaded successfully")
 
-    logging.info("Drone camera loaded successfully")
+            # Load the model once before the loop
+model = load_model(DEFAULT_MODEL_PATH)
 
-    # Load the model once before the loop
-    model = load_model(DEFAULT_MODEL_PATH)
+while True:
+    success, frame = camera.read()
 
-    while True:
-        success, frame = camera.read()
+    if not success:
+        logging.error("Frame couldn't be read from drone camera")
+        break
 
-        if not success:
-            logging.error("Frame couldn't be read from drone camera")
-            break
+    # Predict the class of the current frame
+    prediction = predict_frame(frame, model)
+    dect_frame = detect_person_animal(frame, prediction)
 
-        # Predict the class of the current frame
-        prediction = predict_frame(frame, model)
+    # Overlay the prediction on the frame
+    cv2.putText(dect_frame, prediction, (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.imshow("WildFire Detection Drone Camera", dect_frame)
 
-        # Overlay the prediction on the frame
-        cv2.putText(frame, prediction, (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        cv2.imshow("WildFire Detection Drone Camera", frame)
+    if cv2.waitKey(30) & 0xFF == ord('q'):
+        logging.info("Drone camera turned off")
+        break
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            logging.info("Drone camera turned off")
-            break
-
-    camera.release()
-    cv2.destroyAllWindows()
+camera.release()
+cv2.destroyAllWindows()
